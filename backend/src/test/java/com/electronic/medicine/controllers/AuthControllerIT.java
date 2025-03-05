2326e00dfa57c3d4a577cfedacb893dfcfed1166
Fokusmod <80995808+Fokusmod@users.noreply.github.com>
2025-03-05 17:07:50 +0300

    Test CI

--- backend/src/test/java/com/electronic/medicine/controllers/AuthControllerIT.java
+++ backend/src/test/java/com/electronic/medicine/controllers/AuthControllerIT.java
@@ -4,7 +4,6 @@ import com.electronic.medicine.DTO.JwtRefreshRequest;
 import com.electronic.medicine.utils.JwtProvider;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
